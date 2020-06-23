84b5294faa7e02dd4d88304284ca9bf586483a4d
Alexandr <ieaiaio@bk.ru>
2020-06-23 23:59:53 +0700

    Add general pages

--- app/src/main/java/com/example/picturemaker/MainActivity.java
+++ app/src/main/java/com/example/picturemaker/MainActivity.java
@@ -2,20 +2,34 @@ package com.example.picturemaker;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.picturemaker.ui.main.GalleryFragment;
+import com.example.picturemaker.ui.main.HomeFragment;
+import com.example.picturemaker.ui.main.ProfileFragment;
+import com.example.picturemaker.ui.main.RecentlyFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+//        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        openFragment(HomeFragment.newInstance());
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -24,15 +38,19 @@ public class MainActivity extends AppCompatActivity {
                 switch (item.getItemId()) {
                     case R.id.action_home:
                         Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+                        openFragment(HomeFragment.newInstance());
                         break;
                     case R.id.action_gallery:
                         Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+                        openFragment(GalleryFragment.newInstance());
                         break;
                     case R.id.action_recently:
                         Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+                        openFragment(RecentlyFragment.newInstance());
                         break;
                     case R.id.action_profile:
                         Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+                        openFragment(ProfileFragment.newInstance());
                         break;
                 }
                 return true;
