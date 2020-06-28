a20bd4a876e5764e7e2c6cace13cfdc5bc281923
Alexandr <ieaiaio@bk.ru>
2020-06-28 22:10:10 +0700

    add fields for filter

--- app/src/main/java/com/example/picturemaker/ui/main/FilterGalleryActivity.java
+++ app/src/main/java/com/example/picturemaker/ui/main/FilterGalleryActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.picturemaker.R;
 
@@ -28,6 +30,26 @@ public class FilterGalleryActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        Spinner spin_genre = (Spinner) findViewById(R.id.spin_filter_genre);
+        Spinner spin_act = (Spinner) findViewById(R.id.spin_filter_act);
+        Spinner spin_level = (Spinner) findViewById(R.id.spin_filter_level);
+
+        ArrayAdapter<String> adapter;
+        String[] items;
+        items = new String[]{"Любой", "Абстракционизм", "Анималистика", "Бытовой", "Импрессионизм",
+                "Исторический", "Натюрморт", "Ню", "Пейзаж", "Портрет"};
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        spin_genre.setAdapter(adapter);
+
+        items = new String[]{"Любой", "Пабло Пикассо", "Винсент ван Гог", "Сальвадор Дали", "Рембрандт",
+                "Клод Моне", "Иван Константинович Айвазовский", "Леонардо да Винчи", "Энди Уорхол", "Поль Гоген"};
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        spin_act.setAdapter(adapter);
+
+        items = new String[]{"Любое", "Легко", "Средне", "Сложно"};
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        spin_level.setAdapter(adapter);
 
 
     }
