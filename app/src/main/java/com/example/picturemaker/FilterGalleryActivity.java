8f2c3da63a1f8a43a8fc233ad52f2ac1cb4375f2
Alexandr <ieaiaio@bk.ru>
2020-07-11 12:58:15 +0700

    refactor filter gallery

--- app/src/main/java/com/example/picturemaker/FilterGalleryActivity.java
+++ app/src/main/java/com/example/picturemaker/FilterGalleryActivity.java
@@ -16,13 +16,35 @@ import com.example.picturemaker.R;
 
 public class FilterGalleryActivity extends AppCompatActivity {
 
+    private Spinner spin_genre;
+    private Spinner spin_act;
+    private Spinner spin_level;
+
+    private void RefreshData(){
+        ArrayAdapter<String> adapter;
+        String[] items;
+        items = new String[]{"Любой", "Абстракционизм", "Анималистика", "Бытовой", "Импрессионизм",
+                "Исторический", "Натюрморт", "Ню", "Пейзаж", "Портрет"};
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        this.spin_genre.setAdapter(adapter);
+
+        items = new String[]{"Любой", "Пабло Пикассо", "Винсент ван Гог", "Сальвадор Дали", "Рембрандт",
+                "Клод Моне", "Иван Константинович Айвазовский", "Леонардо да Винчи", "Энди Уорхол", "Поль Гоген"};
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        this.spin_act.setAdapter(adapter);
+
+        items = new String[]{"Любое", "Легко", "Средне", "Сложно"};
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        this.spin_level.setAdapter(adapter);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.filter_gallery);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.filter_gallery_toolbar);
         setSupportActionBar(toolbar);
-
         Button accept_button = (Button) findViewById(R.id.accept_filter);
         accept_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,7 +52,6 @@ public class FilterGalleryActivity extends AppCompatActivity {
                 finish();
             }
         });
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("");
@@ -42,27 +63,10 @@ public class FilterGalleryActivity extends AppCompatActivity {
             }
         });
 
-        Spinner spin_genre = (Spinner) findViewById(R.id.spin_filter_genre);
-        Spinner spin_act = (Spinner) findViewById(R.id.spin_filter_act);
-        Spinner spin_level = (Spinner) findViewById(R.id.spin_filter_level);
+        this.spin_genre = (Spinner) findViewById(R.id.spin_filter_genre);
+        this.spin_act = (Spinner) findViewById(R.id.spin_filter_act);
+        this.spin_level = (Spinner) findViewById(R.id.spin_filter_level);
 
-        ArrayAdapter<String> adapter;
-        String[] items;
-        items = new String[]{"Любой", "Абстракционизм", "Анималистика", "Бытовой", "Импрессионизм",
-                "Исторический", "Натюрморт", "Ню", "Пейзаж", "Портрет"};
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
-        spin_genre.setAdapter(adapter);
-
-        items = new String[]{"Любой", "Пабло Пикассо", "Винсент ван Гог", "Сальвадор Дали", "Рембрандт",
-                "Клод Моне", "Иван Константинович Айвазовский", "Леонардо да Винчи", "Энди Уорхол", "Поль Гоген"};
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
-        spin_act.setAdapter(adapter);
-
-        items = new String[]{"Любое", "Легко", "Средне", "Сложно"};
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
-        spin_level.setAdapter(adapter);
-
-
+        this.RefreshData();
     }
-
 }
