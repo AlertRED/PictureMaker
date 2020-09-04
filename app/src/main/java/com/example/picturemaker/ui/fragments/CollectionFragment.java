60e013e5f3a9920c26aeff8666fc3753c4039a70
Alexandr <ieaiaio@bk.ru>
2020-09-04 18:48:30 +0300

    move TabFragment to fragments folder

--- app/src/main/java/com/example/picturemaker/ui/fragments/CollectionFragment.java
+++ app/src/main/java/com/example/picturemaker/ui/fragments/CollectionFragment.java
@@ -13,7 +13,6 @@ import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.picturemaker.R;
-import com.example.picturemaker.ui.fragments.collection_tabs.TabFragment;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
